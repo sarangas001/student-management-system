be14abd1d7adb6c5cc4cc6e8350b62e660c6c98d
M A S Samarakoon <sarangasama@gmail.com>
2026-06-24 01:59:07 +0530

    hotfix/Change the vite config to allowed access

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,9 +6,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
   server: {
-    allowedHosts: [
-      "http://a07efaf2e4de44f449d7144b31ba3853-131990732.ap-south-1.elb.amazonaws.com",
-      "http://a3b33f38c7124489294eef3116ba6f34-1750585107.ap-south-1.elb.amazonaws.com"
-    ]
+    server: {
+      host: '0.0.0.0',
+      allowedHosts: true
+    }
   }
 })
