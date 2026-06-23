4e5bd6f0860707e3831690c9ffe949489bcd8150
M A S Samarakoon <sarangasama@gmail.com>
2026-06-24 02:04:11 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,10 @@ export default defineConfig({
   server: {
     server: {
       host: '0.0.0.0',
-      allowedHosts: true
+      allowedHosts: [
+        'a07efaf2e4de44f449d7144b31ba3853-131990732.ap-south-1.elb.amazonaws.com',
+        'a3b33f38c7124489294eef3116ba6f34-1750585107.ap-south-1.elb.amazonaws.com'
+      ]
     }
   }
 })
