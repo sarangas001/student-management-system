a3de5b97bbb0f4ee38f2b27306b3c1ec0c3c49e0
saranaga <sarangasama@gmail.com>
2026-06-26 01:03:13 +0530

    Completed the getTeacherSchedule in teacher Schedule

--- server/routes/teacherScheduleRoutes.js
+++ server/routes/teacherScheduleRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const teacherScheduleController = require('../controllers/teacherScheduleController');
 
 router.get('/', teacherScheduleController.getTeacherSchedule);
+router.get('/:teacherId', teacherScheduleController.getTeacherSchedule);
 
 module.exports = router;
