87211b708b91f1886d96ff374b7ad097d5de65ce
Rashmi Ranasinghe <rpranasinghe1104@gmail.com>
2026-06-26 00:24:16 +0530

    Implement course management module with CRUD functionality

--- server/controllers/adminCoursesController.js
+++ server/controllers/adminCoursesController.js
@@ -1,23 +1,81 @@
 
+const Course = require("../module/courseModel");
+
+// Get All Courses
 const getAllCourses = async (req, res) => {
+  try {
+    const courses = await Course.find().populate("teacher");
 
+    res.status(200).json(courses);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
+// Create Course
 const createCourse = async (req, res) => {
+  try {
+    const course = await Course.create(req.body);
 
+    res.status(201).json(course);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
+// Update Course
 const updateCourse = async (req, res) => {
+  try {
+    const updatedCourse = await Course.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
 
+    if (!updatedCourse) {
+      return res.status(404).json({
+        message: "Course not found",
+      });
+    }
+
+    res.status(200).json(updatedCourse);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
+// Delete Course
 const deleteCourse = async (req, res) => {
-  
+  try {
+    const deletedCourse = await Course.findByIdAndDelete(
+      req.params.id
+    );
+
+    if (!deletedCourse) {
+      return res.status(404).json({
+        message: "Course not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Course deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
 module.exports = {
   getAllCourses,
   createCourse,
   updateCourse,
-  deleteCourse
+  deleteCourse,
 };
