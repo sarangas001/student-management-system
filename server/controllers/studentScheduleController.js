f88c786ac5baa20760a738384572035add7691f7
dinithiras <dinithisri2002@gmail.com>
2026-06-28 17:19:29 +0530

    feat: implement student schedule API

--- server/controllers/studentScheduleController.js
+++ server/controllers/studentScheduleController.js
@@ -1,5 +1,31 @@
+const Student = require("../module/studentModel");
+
 const getStudentSchedule = async (req, res) => {
-  
+  try {
+    const { studentId } = req.query;
+
+    const student = await Student.findOne({ studentId })
+      .populate({
+        path: "enrolledCourses",
+        populate: {
+          path: "teacher",
+          select: "firstName lastName"
+        }
+      });
+
+    if (!student) {
+      return res.status(404).json({
+        message: "Student not found"
+      });
+    }
+
+    res.json(student.enrolledCourses);
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
 };
 
 module.exports = {
